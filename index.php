e3e33ab66f095763b2357818906d378701c48e91
firdausjr <104861554+firdausjr@users.noreply.github.com>
2022-05-12 22:06:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<html><head><center>
+<link rel="icon" href="https://play-lh.googleusercontent.com/kWzxVAQlM-CPjT5J_do0GR5IJn7TPCr6zXzPaASmcVhgtiNFVmp2bYO9Q_JS94LELks" type="image/gif" />
+<title>Autoscript.net </title><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta property="og:title" content=""><meta property="og:description" content="DaysVPN"/><meta content="" name="Abstract"><meta name=""><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style type="text/css">/* latin */ @font-face { font-family: 'Iceberg'; font-style: normal; font-weight: 400; src: local('Iceberg'), local('Iceberg-Regular'), url(https://fonts.gstatic.com/s/iceberg/v7/8QIJdijAiM7o-qnZiI8EqprnEO0.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style>
+<style type="text/css">/* latin */@font-face { font-family: 'Wallpoet'; font-style: normal; font-weight: 400; src: local('Wallpoet'), url(https://fonts.gstatic.com/s/wallpoet/v11/f0X10em2_8RnXVVdUObp58Tt868H.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}</style>
+<style type="text/css"> @import url('https://fonts.googleapis.com/css?family=Nunito');@import url('https://fonts.googleapis.com/css?family=Poiret+One');body, html {height: 100%;background-repeat: no-repeat; /*background-image: linear-gradient(rgb(12, 97, 33),rgb(104, 145, 162));*/background:black;position: relative;}#particles-js{ width: 100%; height: 100%; background-size: cover; background-position: 50% 50%; position: fixed; top: 0px; z-index:1;}h1{font:1.5em Cinzel,serif;font-weight:400;letter-spacing:.35em;text-shadow:0 0 25px rgba(254,254,255,.85);width:70%} } footer { margin: 20%; } #a { onmousedown:stop; animation-name: rotate ; animation-duration: 5s; animation-play-state: running; animation-timing-function: linear; animation-iteration-count: infinite; opacity: 1.0;filter: alpha(opacity=50);} img:hover {opacity: 1.0;filter: alpha(opacity=100);} @keyframes rotate{ 10% {transform:rotateY(36deg)} 20% {transform:rotateY(72deg)} 30% {transform:rotateY(108deg)} 40% {transform:rotateY(144deg)} 50% {transform:rotateY(180deg)} 60% {transform:rotateY(216deg)} 70% {transform:rotateY(252deg)} 80% {transform:rotateY(288deg)} 90% {transform:rotateY(324deg)} 100% {transform:rotateY(360deg)} }</style></head>
+
+<body>
+
+<!--- Whatsapp ---->
+<div style="position:fixed;left:35px;bottom:35px;">
+<a href="https://wa.me/6287891876989">
+<img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" width="50" height="50"></button></a>
+</div>
+
+<!--- Telegram ---->
+<div style="position:fixed;right:35px;bottom:35px;">
+<a href="https://t.me/DaysVPN">
+<img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Telegram_2019_Logo.svg" width="43" height="49""></button></a>
+</div>
+
+<style type="text/css">.blink_me{font-size:40px;-webkit-animation-name:blinker;-webkit-animation-duration:2s;-webkit-animation-timing-function:linear;-webkit-animation-iteration-count:infinite;-moz-animation-name:blinker;-moz-animation-duration:2s;-moz-animation-timing-function:linear;-moz-animation-iteration-count:infinite;animation-name:blinker;animation-duration:1.5s;animation-timing-function:linear;animation-iteration-count:infinite;}@-moz-keyframes blinker{0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}@-webkit-keyframes blinker{ 0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}@keyframes blinker{0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}</style><br><br>
+<blink><center><span class="blink_me"><font size="5" color="Red" face="Wallpoet" style="text-shadow: 5px 0px 30px red;">WELLCOME</span><br><font size="5" color="bluewhite" face="Wallpoet" style="text-shadow: 5px 0px 30px red;">INDONESIA VPN SERVICE PROVIDER STORES</font></center><br>
+<p style="font-family:courier,arial,helvetica;" align="center" >
+    <font size="4" color="red"><b>What is VPN?</b></font><br>
+    <font size="3" color="white">
+    In general, the notion of VPN is a communication network technology that creates a secure network connection through a public network, such as the internet, or a private network owned by a service provider.<br>
+    In other words, VPN is a connection between one network and another private network (private) through a public network (internet).<br><br>
+    <b><strong>The following is an explanation of the meaning of each word in Virtual Private Network:</strong></p></b>
+    <p style="font-family:courier,arial,helvetica;" align="left">
+        <i><b><strong>Virtual :</strong></b> Things that are not real or pseudo, in the virtual world / internet.</i><br>
+        <i><b><strong>Private :</strong></b> Things that are private, cannot be accessed by just anyone. To keep it secret from public networks, all data will be encrypted.</i><br>
+        <i><b><strong>Network :</strong></b> Network of interconnected connections.</i><br>
+</p>
+<p style="font-family:courier,arial,helvetica;" align="center" >
+    So, from the explanation above, it can be concluded that VPN is a connection or connection that occurs virtually on a particular network without an encrypted real connection.
+    Currently there are many VPN service providers that we can find on the internet at affordable prices, one of which is https://vpnstores.firdausjr.xyz By using a VPN, your personal data and online activities will be securely encrypted so that it cannot be tracked or stolen by other parties. <br><br><br>
+    <font size="5" color="red"><b>~ WEBSITE IS IN DEVELOPMENT ~</b></font>
+</p>
+<b><b><div id="particles-js"><canvas class="particles-js-canvas-el" style="width: 100%; height: 100%;" width="1365" height="949"></canvas></div>
+<!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js"></script>-->
+<script type="text/javascript">$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function(){ particlesJS('particles-js', { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 5, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true, "config_demo": { "hide_card": false, "background_color": "#b61924", "background_image": "", "background_position": "50% 50%", "background_repeat": "no-repeat", "background_size": "cover" } } );}); </script>
+<script> // This script and many more from // http://rainbow.arch.scriptmania.comif (document.getElementById){ // Plenty of black gives a better sparkle effect.showerCol=new Array('#000000','#ff0000','#ffffff','#000000','#00ff00','#ff00ff','#ffffff','#ffa500','#000000','#fff000');launchCol=new Array('#ffff00','#ff00ff','#00ffff','#ffffff','#ff8000');runSpeed=70; //setTimeout speed.// *** DO NOT EDIT BELOW ***var yPos=200;var xPos=200;var explosionSize=200;var launchColour='#ffff80';var timer=null;var dims=8;var evn=360/14;firework=new Array();var ieType=(typeof window.innerWidth != 'number');var ieRef=((ieType) && (document.compatMode) && (document.compatMode.indexOf("CSS") != -1))?document.documentElement:document.body;thisStep=0;step=5;for (i=0; i < 14; i++){document.write(' <div id="sparks'+i+'" style="position:absolute;top:0px;left:0px;border-radius:50%;height:'+dims+'px;width:'+dims+';font-size:'+dims+';background-color:'+launchColour+'"> &lt;\/div&gt;'); firework=document.getElementById(&quot;sparks&quot;+i).style; } function winDims(){ winH=(ieType)?ieRef.clientHeight:window.innerHeight; winW=(ieType)?ieRef.clientWidth:window.innerWidth; bestFit=(winW &gt;= winH)?winH:winW; } winDims(); window.onresize=new Function(&quot;winDims()&quot;); function Reset(){ var dsy=(ieType)?ieRef.scrollTop:window.pageYOffset; thisStep=-1; launchColour = launchCol[Math.floor(Math.random()*launchCol.length)]; explosionSize=Math.round(100+Math.random()*(bestFit/4)); yPos = explosionSize+Math.round(Math.random()*(winH-(explosionSize*2.2)))+dsy; xPos = explosionSize+Math.round(Math.random()*(winW-(explosionSize*2.2))); for (i=0; i &lt; 14; i++){ firework.backgroundColor=launchColour; firework.width=dims+&quot;px&quot;; firework.height=dims+&quot;px&quot;; firework.fontSize=dims+&quot;px&quot;; } Fireworks(); } function Fireworks(){ thisStep+=step; timer=setTimeout(&quot;Fireworks()&quot;,runSpeed); for (i=0; i &lt; 14; i++){ firework.top = yPos + explosionSize * Math.sin(i*evn*Math.PI/180)*Math.sin(thisStep/100)+&quot;px&quot;; firework.left= xPos + explosionSize * Math.cos(i*evn*Math.PI/180)*Math.sin(thisStep/100)+&quot;px&quot;; if (thisStep &gt; 100){ var dims_change=(explosionSize &lt; 150)?dims:Math.round(dims+Math.random()*2); firework.backgroundColor=showerCol[Math.floor(Math.random()*showerCol.length)]; firework.width=dims_change+&quot;px&quot;; firework.height=dims_change+&quot;px&quot;; firework.fontSize=dims_change+&quot;px&quot;; } } if (thisStep &gt; 140){ clearTimeout(timer); Reset(); } } window.onload=Fireworks; } </div> </script></b>
+<br>
+
+<center><marquee scrollamount="5" width="100%">
+<font size="2" color="lightblue" face="Wallpoet">OFFICIAL TEAM   |   <font size="2" color="white" face="Iceberg">Days - Firdaus - Neza - Afdhan - Horras - Wildy - Ajun - All gretongers indonesia</font></marquee></font>
+</center>
+
+</body>
+
+<br>
+    <span class="blink_me"><font size="4" color="Red" face="Wallpoet" style="text-shadow: 5px 0px 27px red;">Powered By Gretongers indonesia <br>
+    <a href="https://fb.com/mdaihasbunallah">COPYRIGHT © 2021<br></span>
+</center>
+
+</html>
